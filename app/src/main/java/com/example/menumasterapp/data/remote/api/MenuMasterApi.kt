d35c73c367daed49604b936ee6b12b3fb912fef6
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2024-06-13 20:37:41 +0300

    Api interface updated

--- app/src/main/java/com/example/menumasterapp/data/remote/api/MenuMasterApi.kt
+++ app/src/main/java/com/example/menumasterapp/data/remote/api/MenuMasterApi.kt
@@ -1,10 +1,16 @@
 package com.example.menumasterapp.data.remote.api
 
+import com.example.menumasterapp.data.remote.api.body.GenerateRecipesBody
 import com.example.menumasterapp.data.remote.api.body.LoginBody
 import com.example.menumasterapp.data.remote.api.response.LoginResponse
+import com.example.menumasterapp.domain.model.Meal
 import com.example.menumasterapp.domain.model.UserInfo
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MenuMasterApi {
 
@@ -13,4 +19,11 @@ interface MenuMasterApi {
 
     @POST("User/Login")
     suspend fun loginUser(@Body loginBody : LoginBody): LoginResponse
+
+    @POST("User/PostRecipes")
+    suspend fun generateMeals(@Header("Authorization") accessToken: String, @Body generateRecipesBody: GenerateRecipesBody): List<Meal>
+
+    @GET("User/GetSavedRecipes")
+    suspend fun getGeneratedMeals(@Header("Authorization") accessToken: String, @Query("Id") id : Int = 30) : List<Meal>
+
 }
